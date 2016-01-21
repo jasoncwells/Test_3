ef1ef50570e6015752da83454857d5c7e242ca1d
jasoncwells <jasoncarlwells@yahoo.co.uk>
2016-01-21 12:22:25 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,11 @@ html = scraperwiki.scrape("http://uk.soccerway.com/teams/netherlands/fortuna-sit
 root = lxml.html.fromstring(html)
 #use .cssselect method on root to grab 'td' tags and put into tds
 tds = root.cssselect('td')
+indexno=0
 for td in tds:
+  indexno=indexno+1
+  record={"td":td.text,"index":indexno}
+  scraperwiki.sqlite.save(["index"],record
   print lxml.html.tostring(td)
   print td.text
 # # Write out to the sqlite database using scraperwiki library
